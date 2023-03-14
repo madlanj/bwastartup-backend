d1ec189e561dabfc25eaae1d60333c05fa2d35c5
Muhammad Adlan Jazuli <adlan.jazuli@nds.co.id>
2023-03-14 16:35:33 +0700

    init project. register user end point

---
+++ handler/user.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"bwastartup/helper"
+	"bwastartup/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler{
+	return &userHandler{userService }
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context){
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors" : errors}
+
+		response := helper.APIResponse("Register Account Failed", http.StatusUnprocessableEntity, "failed", errorMessage)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+	if err != nil {
+		response := helper.APIResponse("Register Account Failed", http.StatusBadRequest, "failed", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	
+	formatter := user.FormatUser(newUser, "tokenJWT")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "successs", formatter)
+	c.JSON(http.StatusOK, response)
+}
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil{
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+
+	userhandler := handler.NewUserHandler(userService)
+	
+	router := gin.Default()
+
+	api := router.Group("api/v1")
+
+	api.POST("/users", userhandler.RegisterUser)
+
+	router.Run()
+	
+	
+	// userInput := user.RegisterUserInput{}
+	// userInput.Name = "Test simpan dari service"
+	// userInput.Email = "contoh@gmail.com"
+	// userInput.Occupation = "anak band"
+	// userInput.Password = "password"
+
+	// userService.RegisterUser(userInput)
+
+	// user := user.User{
+	// 	Name:"Test simpan",
+	// }
+
+	// userRepository.Save(user)
+
+}
