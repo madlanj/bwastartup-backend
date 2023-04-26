1f5d71e1642d048f6ee6eae65fb8e0bbca2ac6cc
Muhammad Adlan Jazuli <adlan.jazuli@nds.co.id>
2023-04-26 17:48:11 +0700

    create campaign enpoint

--- campaign/repository.go
+++ campaign/repository.go
@@ -6,7 +6,7 @@ type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserId(userID int) ([]Campaign, error)
 	FindByID(ID int) (Campaign, error)
-
+	Save(campaign Campaign)(Campaign, error)
 }
 
 type repository struct {
@@ -47,6 +47,16 @@ func (r *repository) FindByID(ID int) (Campaign, error){
 
 	if err != nil {
 		return campaign, err 
+	}
+
+	return campaign, nil
+}
+
+func (r *repository) Save(campaign Campaign) (Campaign, error){
+	err := r.db.Create(&campaign).Error
+
+	if err != nil { 
+		return campaign, err
 	}
 
 	return campaign, nil
