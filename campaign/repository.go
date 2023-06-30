20805b274663654205f9433f4e4227bb03ed48e0
Muhammad Adlan Jazuli <adlan.jazuli@nds.co.id>
2023-06-30 20:56:16 +0700

    upload campaign image endpoint

--- campaign/repository.go
+++ campaign/repository.go
@@ -8,6 +8,8 @@ type Repository interface {
 	FindByID(ID int) (Campaign, error)
 	Save(campaign Campaign)(Campaign, error)
 	Update(campaign Campaign)(Campaign, error)
+	CreateImage(campaignImage CampaignImage) (CampaignImage, error)
+	MarkAllImagesNonPrimary(campaignId int)(bool, error)
 }
 
 type repository struct {
@@ -71,4 +73,24 @@ func (r *repository) Update(campaign Campaign) (Campaign, error){
 	}
 
 	return campaign, nil
+}
+
+func (r *repository) CreateImage(campaignImage CampaignImage) (CampaignImage, error){
+	err := r.db.Create(&campaignImage).Error
+
+	if err != nil { 
+		return campaignImage, err
+	}
+
+	return campaignImage, nil
+}
+
+func (r *repository) MarkAllImagesNonPrimary(campaignId int)(bool, error){
+	// UPDATE campaign_images SET is_primary = false WHERE campaign_id=1
+	err := r.db.Model(&CampaignImage{}).Where("campaign_id = ?", campaignId).Update("is_primary", false).Error
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
